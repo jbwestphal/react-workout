938b3ff4cfe2f1586d2cf515aa6d972bbd1af594
Jessica Westphal <pretty.jehh.s2@hotmail.com>
2018-01-28 00:16:03 -0300

    New component created, included React navigation and a calendar component

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { View, Platform } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
 import History from './components/History'
+import { TabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 
 export default class App extends React.Component {
   render() {
